6773650d072db621860fd6508464821e6b61343d
qq442722405 <48858630+qq442722405@users.noreply.github.com>
2026-06-23 21:21:08 +0800

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,21 +1,16 @@
-import numpy as np
 import cv2
+import numpy as np
 from paddleocr import PaddleOCR
 
-ocr = PaddleOCR(
-    use_angle_cls=False,
-    lang="en",
-    show_log=False
-)
+ocr = PaddleOCR(use_angle_cls=False, lang="en", show_log=False)
 
 
 def read_number(img):
 
-    arr = np.array(img)
+    img = np.array(img)
 
-    img = cv2.cvtColor(arr, cv2.COLOR_BGRA2BGR)
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+    gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
 
     gray = cv2.resize(gray, None, fx=2, fy=2)
 
@@ -25,10 +20,6 @@ def read_number(img):
 
     try:
         text = result[0][0][1][0]
-
-        text = text.replace(" ", "").replace(",", ".")
-
         return float(text)
-
     except:
         return None
